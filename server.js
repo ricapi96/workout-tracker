3a3f2ac1035f82d0e4d1d36c35ba879a257730ba
Erica Roquemore <enroquemore@gmail.com>
2021-06-22 02:24:39 -0400

    changed to localhost

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(routes);
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/infinite-savannah", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
