c1036e89ff30b93c812b9c84debce55163de478a
Erica Roquemore <enroquemore@gmail.com>
2021-06-22 01:08:08 -0400

    deploying

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(routes);
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/infinite-savannah", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
