f0c310f9f52be81eabe4056af2f8988a0a33640f
Erica Roquemore <enroquemore@gmail.com>
2021-06-22 22:42:10 -0400

    renamed routes file

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 
 const apiRoutes = require("../routes/api");
-const homeRoutes = require("./home-routes");
+const homeRoutes = require("./html.routes");
 
 router.use("/api", apiRoutes);
 router.use("/", homeRoutes);
